33fcb6f24a35cf84e73289032fe1e24dc45a93aa
Ezgi <144616221+ecayy@users.noreply.github.com>
2024-04-22 18:53:41 +0300

    Add files via upload

---
+++ Calculator/src/Calculator.java
@@ -0,0 +1,19 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Calculator {
+    int boardWidht = 500;
+    int boardHeight = 500;
+
+    JFrame frame = new JFrame("Calculator");
+    JLabel textLabel = new JLabel();
+    JPanel textPanel = new JPanel();
+    JPanel boardPanel = new JPanel();
+
+
+    Calculator() {
+        
+    }
+    
+}
